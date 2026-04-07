f0145153d1c3b63df073c7ecf6c4aff14b3e126e
bansodeprashant150 <bansodeprashant150@gmail.com>
2026-04-07 23:37:02 +0530

    Refactor UniquePairs for clarity and functionality
    Refactor UniquePairs class to improve variable naming and structure. Adjust input handling for pairs and maintain unique pairs in a HashSet.

--- HashSet/UniquePairs.java
+++ HashSet/UniquePairs.java
@@ -1,23 +1,31 @@
-package HashSet;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-import java.util.HashSet;
-import java.util.Scanner;
+public class Solution {
 
-public class UniquePairs {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-        int n=sc.nextInt();
-        sc.nextLine();
-        HashSet<String> set=new HashSet<>();
-        for(int i=0; i<n;i++){
-
-            String pair=sc.nextLine();
-            set.add(pair);
-            System.out.println(set.size());
-
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
         }
 
-        
+//Write your code here
+HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < t; i++) {
+            String pair = (pair_left[i].compareTo(pair_right[i]) <= 0)
+                    ? (pair_left[i] + " " + pair_right[i])
+                    : (pair_right[i] + " " + pair_left[i]);
+            set.add(pair);
+            System.out.println(set.size());
+        }
     }
 }
