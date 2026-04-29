16e892fcd4174c4e65b0505a1ab8e8c798c13f16
bansodeprashant150 <bansodeprashant150@gmail.com>
2026-04-29 21:31:21 +0530

    Create Subsequence.java

---
+++ Two Pointers/Subsequence.java
@@ -0,0 +1,23 @@
+class Subsequence {
+    public boolean isSubsequence(String s, String t) {
+
+        int i=0;
+        int j=0;
+        while(i<s.length()&&j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                
+            }
+            j++;
+         
+    }
+    return i==s.length();}
+    public static void main(String[] args) {
+
+        String s="abc";
+        String t="ahbgdc";
+        Subsequence obj=new Subsequence();
+        boolean result=obj.isSubsequence(s, t);
+        System.out.println(result);
+    }
+}
