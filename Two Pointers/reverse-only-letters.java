678e2cd7f4e1186663280d9e6030324f75a512a5
bansodeprashant150 <bansodeprashant150@gmail.com>
2026-05-10 19:35:12 +0530

    Create reverse-only-letters.java

---
+++ Two Pointers/reverse-only-letters.java
@@ -0,0 +1,36 @@
+public class reverse-only-letters {
+    public String reverseOnlyLetters(String s) {
+      
+      char[]arr=s.toCharArray();
+        int left=0;
+        int right=s.length()-1;
+        
+       while(left < right){
+
+    if(!Character.isLetter(arr[left])){
+        left++;
+    }
+
+    else if(!Character.isLetter(arr[right])){
+        right--;
+    }
+
+    else{
+
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+}
+              return new String(arr);
+    }
+    public static void main(String[]args){
+      String s="a-bC-dEf-ghIj";
+      reverse-only-letters obj=new reverse-only-letters();
+      String result=obj.reverseOnlyLetters(s);
+      System.out.println(result);
+      }
+}
