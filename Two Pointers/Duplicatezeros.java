c5800a89d1a2dc4c603c960f39e35d25c9c280c2
Prashant Bansode <bansodeprashant150@gmail.com>
2026-05-15 21:28:40 +0530

    Create Duplicatezeros.java

---
+++ Two Pointers/Duplicatezeros.java
@@ -0,0 +1,22 @@
+public class Duplicatezeros {
+     public void duplicateZeros(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+          if(i!=arr.length){
+            if(arr[i]==0){
+                for(int j=arr.length-1;j>i;j--){
+                  arr[j]=arr[j-1];
+                  }
+                  i++;
+            }}
+            
+        }
+        for(int num:arr){
+                System.out.print(num+" ");
+            }
+    }
+    public static void main(String[] args) {
+        int[]arr={1,0,2,3,0,4,5,0};
+        Duplicatezeros obj= new Duplicatezeros();
+        obj.duplicateZeros(arr);
+    }
+}
