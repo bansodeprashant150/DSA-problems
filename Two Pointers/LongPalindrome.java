def78cd90d3895dc439ce0bd70de972ae96ddbca
Prashant Bansode <bansodeprashant150@gmail.com>
2026-05-23 23:18:21 +0530

    Create LongPalindrome.java

---
+++ Two Pointers/LongPalindrome.java
@@ -0,0 +1,46 @@
+public class LongPalindrome {
+
+    public String longestPalindrome(String s) {
+
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            String odd = expand(s, i, i);
+
+            String even = expand(s, i, i + 1);
+
+            if (odd.length() > ans.length()) {
+                ans = odd;
+            }
+
+            if (even.length() > ans.length()) {
+                ans = even;
+            }
+        }
+
+        return ans;
+    }
+
+    public String expand(String s, int left, int right) {
+
+        while (left >= 0 &&
+               right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+
+            left--;
+            right++;
+        }
+
+        return s.substring(left + 1, right);
+    }
+
+    public static void main(String[] args) {
+
+        LongPalindrome obj = new LongPalindrome();
+
+        String s = "babad";
+
+        System.out.println(obj.longestPalindrome(s));
+    }
+}
