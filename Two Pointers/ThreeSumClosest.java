a9daf6199ad79f6dfacf907c264582d836b8015e
Prashant Bansode <bansodeprashant150@gmail.com>
2026-05-29 23:15:56 +0530

    Implement ThreeSumClosest algorithm

---
+++ Two Pointers/ThreeSumClosest.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(target - currentSum) < Math.abs(target - closestSum)) {
+                    closestSum = currentSum;
+                }
+
+                if (currentSum < target) {
+                    left++;
+                } else if (currentSum > target) {
+                    right--;
+                } else {
+                    return currentSum; // Exact match found
+                }
+            }
+        }
+
+        return closestSum;
+    }
+
+    public static void main(String[] args) {
+        ThreeSumClosest solver = new ThreeSumClosest();
+
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+
+        int result = solver.threeSumClosest(nums, target);
+
+        System.out.println("Closest sum to target is: " + result);
+    }
+}
